90091d9b166910ba78f4f5f656dad984209bca33
zjp-zjp-zjp <80824286+zjp-zjp-zjp@users.noreply.github.com>
2021-11-29 16:33:29 +0800

    Add files via upload

---
+++ jilijili/src/main/java/com/example/jilijili/JilijiliApplication.java
@@ -0,0 +1,13 @@
+package com.example.jilijili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JilijiliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JilijiliApplication.class, args);
+    }
+
+}
---
+++ jilijili/src/test/java/com/example/jilijili/JilijiliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jilijili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JilijiliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
