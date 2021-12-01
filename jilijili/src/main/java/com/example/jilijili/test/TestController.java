0637df44d5f5ac160586332b37ca4d11e06fe433
littleblueprince <58162001+littleblueprince@users.noreply.github.com>
2021-12-01 18:13:57 +0800

    实现了加入css和js的动态网页

--- jilijili/src/main/java/com/example/jilijili/test/TestController.java
+++ jilijili/src/main/java/com/example/jilijili/test/TestController.java
@@ -1,7 +1,18 @@
 package com.example.jilijili.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+@Controller
 public class TestController {
-
-
+    @RequestMapping("/test")
+    public ModelAndView sayHello(HashMap<String, Object> map)    {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("test");//动态网页名称
+        modelAndView.addObject("key", 12345);
+        return modelAndView;
+    }
 
 }
