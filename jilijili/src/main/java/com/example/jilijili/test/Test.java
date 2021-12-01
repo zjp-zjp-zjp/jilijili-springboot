7c12c2cf5903c18a5000f240e38e0ad7f95e962f
zjp-zjp-zjp <914856774@qq.com>
2021-12-02 02:13:22 +0800

    Revert "rubbish"
    This reverts commit a450682796cdc8195ed31e3ef4a667bb83670ad2.

--- jilijili/src/main/java/com/example/jilijili/test/Test.java
+++ jilijili/src/main/java/com/example/jilijili/test/Test.java
@@ -1,14 +1,4 @@
 package com.example.jilijili.test;
 
 public class Test {
-    String name;
-    Integer id;
-
-    public Test() {
-    }
-
-    public Test(String name, Integer id) {
-        this.name = name;
-        this.id = id;
-    }
 }
