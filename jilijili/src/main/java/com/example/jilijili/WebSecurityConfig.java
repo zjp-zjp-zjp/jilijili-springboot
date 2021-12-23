235978e2b5b71b010c9d31b9f916782a900ae806
zjp-zjp-zjp <914856774@qq.com>
2021-12-23 22:59:27 +0800

    纠正index.html bug

--- jilijili/src/main/java/com/example/jilijili/WebSecurityConfig.java
+++ jilijili/src/main/java/com/example/jilijili/WebSecurityConfig.java
@@ -13,5 +13,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests().anyRequest().permitAll();
+        http.headers().frameOptions().sameOrigin();
     }
 }
